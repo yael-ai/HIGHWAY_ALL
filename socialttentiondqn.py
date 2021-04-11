e545156ef0805baa489aa9fa93c6eceebc0f8212
yael-ai <74661157+yael-ai@users.noreply.github.com>
2021-04-11 11:48:25 +0300

    3!

--- socialttentiondqn.py
+++ socialttentiondqn.py
@@ -63,7 +63,7 @@ env.reset()
 agent = load_agent(agent_config, env)
 evaluation = Evaluation(env, agent, num_episodes=3000, display_env=False)
 print("11/04/2021")
-print("git work!!")
+print("git work!!!")
 print(f"Ready to train {agent} on {env}")
 
 """Run tensorboard locally to visualize training."""
